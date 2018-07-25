2ac04e31187452941b63afcffbd30e5fc0de9067
JUN JIAN SHEN <jshen9085@gmail.com>
2018-07-25 02:35:39 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
-def oxford_comma(array)
+# def oxford_comma(array)
+#   if array.count == 1
+#   return array.join
+  
+#   elsif array.count == 2 
+#   return array.join(" and "
+# end
 
+ 
+def oxford_comma(array)
+  if array.count == 1
+  	array.join
+  	
+  elsif array.count == 2
+    array.join(" and ")
+  
+  elsif array.length > 2
+    array[-1].prepend "and "
+	array.join(", ")
+  end
 end
