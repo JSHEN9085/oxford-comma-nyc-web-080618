766dba1fd81a87c0fb85e2284f776cefe4632dc2
JUN JIAN SHEN <jshen9085@gmail.com>
2018-07-25 02:28:03 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,7 @@ def oxford_comma(array)
   if array.count == 1
   return array.join
   
-  elsif array.count ==2 
+  elsif array.count == 2 
   return array.join(" and ")
-  
-  elsif array.count == 3
   
 end
