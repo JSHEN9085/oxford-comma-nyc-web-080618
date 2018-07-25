4efc9bc5802e53bc97294367c2ee786d30ec7e12
JUN JIAN SHEN <jshen9085@gmail.com>
2018-07-25 02:30:18 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,17 +3,17 @@
 #   return array.join
   
 #   elsif array.count == 2 
-#   return array.join(" and ")
-  array = ["apple"]
+#   return array.join(" and "
 # end
 
-# 
+ 
 def oxford_comma(array)
-  if array.length == 2
-  array[-2] << " and "
-  array.join
-  elsif array.length == 1
+  if array.length == 1
   	array.join
+  	
+  elsif array.length == 2
+    array.join(" and ")
+  
   elsif array.length > 2
     array[-1].prepend "and "
 	array.join(", ")
